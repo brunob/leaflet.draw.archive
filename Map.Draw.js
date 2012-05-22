bf1493c1fd42068f330bb213a88c5e8201d73ef2
abusquets <abusquets@gmail.com>
2012-05-22 11:50:53 +0200

    Added draw marker and draw rectangle.

--- Map.Draw.js
+++ Map.Draw.js
@@ -14,24 +14,39 @@ L.Map.Draw = L.Handler.extend({
 	},
 	
 	addHooks: function () {
-		if(this._type == 'polygon')
-			this._poly = new L.Polygon([]);
-		else
-			this._poly = new L.Polyline([]);
-		
-		this._initMarkers();
-		this._map.addLayer(this._markerGroup);
-		this._map.addLayer(this._poly);
-		this._map.on('click', this._drawPoly);
+		if(this._type == 'marker'){
+			this._marker = null;
+			this._map.on('click', this._drawMarker);			
+		}else{
+			
+			if(this._type == 'polygon'){
+				this._poly = new L.Polygon([]);
+			}else if(this._type == 'rectangle'){
+				this._poly = new L.Polygon([]);		
+			}else{
+				this._poly = new L.Polyline([]);
+			}
+			
+			this._initMarkers();
+			this._map.addLayer(this._markerGroup);
+			this._map.addLayer(this._poly);
+			this._map.on('click', this._drawPoly);
+		}
 	},
 
 	removeHooks: function () {
-		this._map.fire("drawend", {poly: this._poly});
-		this._map.removeLayer(this._markerGroup);
-		delete this._markerGroup;
-		delete this._markers;
-		delete this._poly;
-		this._map.off('click', this._drawPoly);
+		if(this._type == 'marker'){
+			this._map.fire("drawend", {marker: this._marker});
+			this._map.off('click', this._drawMarker);	
+			delete this._marker;
+		}else{
+			this._map.fire("drawend", {poly: this._poly});
+			this._map.removeLayer(this._markerGroup);
+			delete this._markerGroup;
+			delete this._markers;
+			delete this._poly;
+			this._map.off('click', this._drawPoly);			
+		}
 	},
 
 	setType: function (type) {
@@ -49,13 +64,16 @@ L.Map.Draw = L.Handler.extend({
 		var latlngs = this._poly._latlngs,
 		    i, len, marker;
 
+
 		for (i = 0, len = latlngs.length; i < len; i++) {
 			marker = this._createMarker(latlngs[i], i);
 			this._markers.push(marker);
 		}
+		
 	},
 	
 	_createMarker: function (latlng) {
+
 		var marker = new L.Marker(latlng, {
 			icon: this.options.icon
 		});
@@ -70,8 +88,23 @@ L.Map.Draw = L.Handler.extend({
 	_drawPoly: function (e) {
 		this.draw._poly.addLatLng(e.latlng);
 		this.draw._markerGroup.clearLayers();
-		this.draw._initMarkers();
+		if((this.draw._type == 'rectangle') && ( this.draw._poly.getLatLngs().length ==2 ) ){
+			var rectangle = new L.Rectangle(this.draw._poly.getBounds());
+	        this.draw._poly.setLatLngs(rectangle.getLatLngs());
+	        rectangle = null;
+	        this.draw.disable();
+		}else{			
+			this.draw._initMarkers();
+		}
+	},
+	
+	_drawMarker: function (e) {
+		this._marker = new L.Marker(e.latlng)
+		this.draw._map.addLayer(this._marker);
+		this.draw.disable();
 	}
+	
+	
 });
 
 L.Map.addInitHook('addHandler', 'draw', L.Map.Draw);
